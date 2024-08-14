5ee71472b3229de7054260f1016e5999da1a884e
SimasJan <simjanusas@gmail.com>
2024-08-14 17:17:03 +0300

    Revmoed header of the Stack Screen

--- src/navigation/AppNavigator.js
+++ src/navigation/AppNavigator.js
@@ -8,9 +8,9 @@ const Stack = createStackNavigator();
 function AppNavigator() {
   return (
     <Stack.Navigator initialRouteName="Home">
-      <Stack.Screen name="Home" component={HomeScreen} />
-      <Stack.Screen name="Camera" component={CameraScreen} />
-      <Stack.Screen name="Results" component={ResultsScreen} />
+      <Stack.Screen name="Home" component={HomeScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Camera" component={CameraScreen} options={{ headerShown: false }}/>
+      <Stack.Screen name="Results" component={ResultsScreen} options={{ headerShown: false }}/>
     </Stack.Navigator>
   );
 }
