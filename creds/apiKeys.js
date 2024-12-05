33eaab7f08c7508c546aaec077e4bbc605942e68
Simas Janusas <Simjanusas@gmail.com>
2024-12-05 22:49:21 +0200

    Update apiKeys.js

--- creds/apiKeys.js
+++ creds/apiKeys.js
@@ -1,3 +1,3 @@
-export const googleVisionApiKey = 'AIzaSyDwWNEKtaQbiEWTBoxIl8i8gFFbgrSiS4w';
-export const geminiApiKey = 'AIzaSyAIp3hZASpM_ATkkG3NAq98ScCKHkDWnR0';
-export const searchEngineID = '41b7ef4e5f78444a6';
+export const googleVisionApiKey = '';
+export const geminiApiKey = '';
+export const searchEngineID = '';
